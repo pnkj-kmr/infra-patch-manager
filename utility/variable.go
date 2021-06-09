cf7a5366b634c7c45edabdf476aed765eb702094
Pankaj Kumar <pankaj.k@everestims.com>
2021-06-10 00:01:21 +0530

    Pre-Check for folder

--- utility/variable.go
+++ utility/variable.go
@@ -2,6 +2,10 @@ package utility
 
 import (
 	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/pnkj-kmr/infra-patch-manager/module/dir"
 )
 
 // RemedyDirectory - default patch location
@@ -22,7 +26,19 @@ func init() {
 		log.Fatal("Cannot load the configuration files: ", err)
 	}
 
+	cwd, _ := os.Getwd()
+	precheck(cwd, config.RemedyDir)
+	precheck(cwd, config.RevokeDir)
+	precheck(cwd, config.AssetsDir)
+
 	RemedyDirectory = config.RemedyDir
 	RevokeDirectory = config.RevokeDir
 	AssetsDirectory = config.AssetsDir
+}
+
+func precheck(cwd, d string) {
+	_, err := dir.New(d)
+	if err != nil {
+		log.Fatal("Create new folder: ", filepath.Join(cwd, d))
+	}
 }
